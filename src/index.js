2b22f288d20e8409270d1acec9fe91eda852f356
coldfear457@gmail.com <coldfear457@gmail.com>
2021-08-11 13:48:58 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,46 +1,85 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
+
+
 function decode(expr) {
-    // write your solution here
+  var count = 10;
+  var pattern = RegExp("((.{" + count + "})+?|(.{1," + count + "})$)", "g");
+  var splitted = expr.match(pattern);
+  const word = [];
+  const words = [];
+
+  for (let i = 0; i < splitted.length; i++) {
+    if (splitted[i][0] == '*') {
+      splitted[i] = ' ';
+    }
+  }
+
+  for (let p = 0; p < splitted.length; p++) {
+    var a = splitted[p].indexOf(1),
+      b = splitted[p].length;
+    c = '';
+    if (b > 1) {
+      splitted[p] = splitted[p].slice(a, b);
+    }
+  }
+
+  var morse = splitted.map(element => {
+    let newElement = element.replace(/(10)/gm, '.')
+    newElement = newElement.replace(/(11)/gm, '-')
+    return newElement;
+  })
+ 
+  for (let y = 0; y < morse.length; y++) {
+    for (let key in MORSE_TABLE) {
+      if (morse[y] == ' '){
+        word[y] = ' ';
+      } else if (key == morse[y]){
+        word[y] = MORSE_TABLE[key];
+      }
+    }
+  }
+  
+  return word.join('');
 }
 
 module.exports = {
-    decode
+  decode
 }
